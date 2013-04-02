f4fa8ca192445ff1e0f04ef983bfb7a726b3baaa
Richard Hollis <richhollis@gmail.com>
2013-04-02 17:02:15 +0100

    Fixed issue with output error: unknown encoding ASCII-8BIT

--- lib/diversion/mailer.rb
+++ lib/diversion/mailer.rb
@@ -3,7 +3,7 @@ require 'mail'
 Mail::Message.class_eval do
   def diversion(*args)
     if html_part
-      html_part.body = Diversion.encode(html_part.body.raw_source)
+      html_part.body = Diversion.encode(html_part.body.raw_source.encode("UTF-8"))
     end
     self
   end
